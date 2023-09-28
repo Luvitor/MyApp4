68cd40482941e34fbb387343102d5dfb61bc3d2b
Luvitor <luan.silva524@etec.sp.gov.br>
2023-09-28 19:47:04 -0300

    Initial commit

---
+++ app/src/main/java/br/com/etecia/myapp/Layout1_3Activity.java
@@ -0,0 +1,14 @@
+package br.com.etecia.myapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Layout1_3Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.exemplo1_3_layout);
+    }
+}
---
+++ app/src/main/java/br/com/etecia/myapp/MainActivity.java
@@ -0,0 +1,14 @@
+package br.com.etecia.myapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.exemplo_1_1_layout);
+    }
+}
